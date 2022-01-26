732a8ffa80efa7a58ea7669c63426deea2a8d098
casa92 <casartelli92@gmail.com>
2022-01-26 18:37:11 +0100

    new database

--- database.php
+++ database.php
@@ -43,32 +43,32 @@
             'genre' => 'Rock'
         ],
         [
-            'title' => 'New Jersey',
-            'author' => 'Bon Jovi',
-            'year' => 1988,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
+            'title' => 'Made in Japan',
+            'author' => 'Deep Purple',
+            'year' => 1972,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg',
             'genre' => 'Rock'
         ],
         [
-            'title' => 'Live at Wembley 86',
-            'author' => 'Queen',
-            'year' => 1992,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
-            'genre' => 'Pop'
+            'title' => 'And Justice for All',
+            'author' => 'Metallica',
+            'year' => 1988,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg',
+            'genre' => 'Metal'
         ],
         [
-            'title' => 'Ten\'s Summoner\'s Tales',
-            'author' => 'Sting',
-            'year' => 1993,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
-            'genre' => 'Pop'
-        ],
-        [
-            'title' => 'Steve Gadd band',
-            'author' => 'Steve Gadd Band',
-            'year' => 2018,
-            'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
+            'title' => 'Hard Wired',
+            'author' => 'Dave Weckl',
+            'year' => 1994,
+            'poster' => 'https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg',
             'genre' => 'Jazz'
+        ],
+        [
+            'title' => 'Bad',
+            'author' => 'Michael Jacjson',
+            'year' => 1987,
+            'poster' => 'https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg',
+            'genre' => 'Pop'
         ]
     ];
 ?>

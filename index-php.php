d39fee7dcf2ad1441cd6e118c1cfd1405cd151d9
casa92 <casartelli92@gmail.com>
2022-01-26 16:55:05 +0100

    first commit

---
+++ index-php.php
@@ -0,0 +1,97 @@
+<?php
+    include __DIR__ . '/database.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Php Ajax Dischi</title>
+</head>
+<body>
+    <header>
+        <div class="header-container">
+            <div class="header-wrapper">
+                <div class="logo">
+                    <img src="img/Spotify_icon.png" alt="Spotify logo">
+                </div>
+                <div>
+                    filtro
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="main-container">
+            <div class="app-wrapper">
+               <div class="single-disc">
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg" alt="">
+                    </div>
+                    <h2>
+                        new jersey
+                    </h2>
+                    <div class="disc-author">
+                        Bon Jovi
+                        <br>
+                        1988
+                    </div>
+               </div>
+               <div class="single-disc">
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg" alt="">
+                    </div>
+               </div>
+               <div class="single-disc">
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg" alt="">
+                    </div>
+               </div>
+               <div class="single-disc">
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg" alt="">
+                    </div>
+               </div>
+               <div class="single-disc">
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg" alt="">
+                    </div>
+               </div>
+               <div class="single-disc">
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg" alt="">
+                    </div>
+               </div>
+               <div class="single-disc">
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg" alt="">
+                    </div>
+               </div>
+               <div class="single-disc">
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg" alt="">
+                    </div>
+               </div>
+               <div class="single-disc">
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg" alt="">
+                    </div>
+               </div>
+               <div class="single-disc">
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg" alt="">
+                    </div>
+               </div>
+            </div>
+
+        </div>
+
+    </main>
+
+
+    
+</body>
+</html>

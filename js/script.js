989336e76e47f6e965a1b8f63aa08e5a01133909
casa92 <casartelli92@gmail.com>
2022-01-27 13:58:33 +0100

    filtro e btn

--- js/script.js
+++ js/script.js
@@ -4,14 +4,25 @@ Vue.config.devtools = true;
 const app = new Vue({
     el: '#root',
     data: {
-        discs: []
+        discs: [],
+        searchText: ''
     },
     methods: {
         getDiscData: function() {
-            axios.get('http://localhost:8888/php-ajax-dischi/server.php')
+            axios.get('http://localhost:8888/php-ajax-dischi/server.php',
+            {
+                params: {
+                    genre: this.searchText
+                }
+            })
+            
             .then((response) => {
                 this.discs = response.data;
             });
+        },
+        resetSearch: function() {
+            this.searchText = '';
+            this.getDiscData();
         }
     },
     created: function() {
--- server.php
+++ server.php
@@ -2,7 +2,26 @@
 
     require __DIR__ . '/database.php';
 
-    $json = json_encode($database);
+    $disc_genre = $_GET['genre'];
+
+    if(empty($disc_genre)) {
+        //se disc genre è vuoto o indefinito stampo tutto l'array
+        $json = json_encode($database);
+    } else {
+        //se il genere è presente mostrerò i dischi del genere corrispondente
+        //filtro i dischi
+        $filtered_discs = [];
+        foreach($database as $disc) {
+            if($disc['genre'] === $disc_genre) {
+                $filtered_discs[] = $disc;
+            }
+        }
+
+        $json = json_encode($filtered_discs);
+    }
+
+    
+    // $json = json_encode($database);
 
     header('Content-Type: application/json');
 
